ce726bc42b3bc339d5ac1428e3060b2d0bf5845e
Anand <anand.r@gormalone.com>
2021-10-18 17:01:04 +0530

    Added a dynamic feature to the app

--- app/src/main/java/com/example/multi_module_demo_app/MainActivity.kt
+++ app/src/main/java/com/example/multi_module_demo_app/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.multi_module_demo_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.multi_module_demo_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonFeature1.setOnClickListener {
+            val intent = Intent("com.example.feature1.FirstFeatureActivity")
+            startActivity(intent)
+        }
     }
 }
---
+++ feature1/src/main/java/com/example/feature1/FirstFeatureActivity.kt
@@ -0,0 +1,11 @@
+package com.example.feature1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FirstFeatureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_feature)
+    }
+}
